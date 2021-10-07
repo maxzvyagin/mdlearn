cc96f880b7a166a1e6abce6c8b8a1a52a5324c9c
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-10-07 10:44:04 -0700

    Update symmetric_conv2d_vae.py

--- mdlearn/nn/models/vae/symmetric_conv2d_vae.py
+++ mdlearn/nn/models/vae/symmetric_conv2d_vae.py
@@ -513,7 +513,7 @@ class SymmetricConv2dVAETrainer(Trainer):
         """
         from mdlearn.data.datasets.contact_map import ContactMapDataset
 
-        if self.input_shape[-1] == 512
+        if self.input_shape[-1] == 512:
             dataset = ContactMapDataset(X, self.input_shape, final_shape=512)
         else:
             dataset = ContactMapDataset(X, self.input_shape)
