d218d122a84b2b4f4923404f708b546a2443bd11
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-11-10 13:00:07 -0700

    batch ize

--- examples/symmetric_conv2d_vae/updated/lightning.py
+++ examples/symmetric_conv2d_vae/updated/lightning.py
@@ -34,7 +34,7 @@ class CVAE(pl.LightningModule):
             dataset,
             0.8,
             "random",
-            batch_size=64,
+            batch_size=128,
             shuffle=True,
             num_workers=10
         )
