9fd920356cc2e65125e33d437d9c9d22d63adbf0
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-11-10 22:43:52 -0700

    back to 512

--- examples/symmetric_conv2d_vae/updated/lightning.py
+++ examples/symmetric_conv2d_vae/updated/lightning.py
@@ -38,7 +38,7 @@ class CVAE(pl.LightningModule):
         print(f"Number of contact maps: {len(contact_maps)}")
         scalars = {"rmsd": scalars}
 
-        dataset = ContactMapDataset(data=contact_maps, shape=self.real_shape, scalars=scalars, pad=True)
+        dataset = ContactMapDataset(data=contact_maps, shape=self.real_shape, scalars=scalars, final_shape=False)
         self.train_loader, self.valid_loader = train_valid_split(
             dataset,
             0.8,
@@ -114,7 +114,7 @@ def lightning():
     torch.set_num_threads(NUM_DATA_WORKERS)
     torch.manual_seed(0)
     input_path_list = glob.glob('/lus/theta-fs0/projects/CVD-Mol-AI/mzvyagin/gordon_bell/anda_newsim_7egq_segmentA/chainA_subset/*.h5')
-    model = CVAE(model_shape=[1, 1024, 1024], real_shape=[1, 926, 926],
+    model = CVAE(model_shape=[1, 512, 512], real_shape=[1, 926, 926],
                  input_path_list=input_path_list)
                  # input_path='/homes/mzvyagin/gordon_bell_processing/anda_newsim_7egq_segmentA/traj_segment_eq.2.10.h5')
     wandb_logger = WandbLogger(project="cvae", entity="mzvyagin", group="ddp")
